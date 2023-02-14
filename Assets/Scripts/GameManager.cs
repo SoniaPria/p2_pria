fa283a555fee2b8483a6f093d52458679f5da894
SoniaPria <webusable@protonmail.com>
2023-02-14 19:07:39 +0100

    Agrega las dos clases

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,8 @@ public class GameManager : MonoBehaviour
 {
    string api = "https://servizos.meteogalicia.gal/mgrss/observacion/jsonCamaras.action";
 
-   //TrivialCs getResults;
-   //public QuestionCs getQuestion;
+   MeteoCamaras getResults;
+   public ListaCamaras getCamaras;
 
    // Plantilla extraída de
    // Unity Docs | UnityWebRequest.Get
@@ -39,39 +39,33 @@ public class GameManager : MonoBehaviour
                     Debug.LogError(pages[page] + ": HTTP Error: " + wr.error);
                 break;
                 case UnityWebRequest.Result.Success:
-                    Debug.Log(pages[page] + ":\nReceived: " + wr.downloadHandler.text);
+                    // Debug.Log(pages[page] + ":\nReceived: " + wr.downloadHandler.text);
 
-                    //CreateFromJSON( wr.downloadHandler.text );
+                    CreateFromJSON( wr.downloadHandler.text );
                 break;
             }
         }
     }
 
-/*
     // JsonUtility.FromJson
     void CreateFromJSON( string jsonString )
     {
-        getResults = JsonUtility.FromJson<TrivialCs>(jsonString);
-        getQuestion = getResults.results[0];
+        getResults = JsonUtility.FromJson<MeteoCamaras>(jsonString);
+        getCamaras = getResults.results[0];
 
         PrintGetQuestion();
     }
+
 
     void PrintGetQuestion()
     {
         // Imprime na consola o número de preguntas solicitadas
         // e 4 datos da primeira pregunta
 
-        int nqq = getResults.results.Count;
+        int ncc = getResults.results.Count;
 
-        print("O número de preguntas solicitadas é: " + nqq );
+        print("O número de cámaras é: " + ncc );
 
-        print("Pregunta 1");
-        print("Categoría " + getResults.results[0].category);
-        print("Tipo " + getResults.results[0].type);
-        print("Dificultad " + getResults.results[0].difficulty);
-        print("Atención Pregunta! " + getResults.results[0].question);
     }
-*/
 }
 
